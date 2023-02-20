50528871af22aa291760314f3ba9827622c7b94c
kkrushkov1 <k.krushkov1@gmail.com>
2023-02-20 19:51:24 +0200

    New solution for an existing task

--- src/Easy/ValidAnagram.java
+++ src/Easy/ValidAnagram.java
@@ -1,6 +1,8 @@
 package Easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ValidAnagram {
 
@@ -10,21 +12,59 @@ public class ValidAnagram {
         System.out.println(isAnagram(s, t));
     }
 
+//    public static boolean isAnagram(String s, String t) {
+//        if (s.length() != t.length()) {
+//            return false;
+//        }
+//
+//        char[] word1 = s.toCharArray();
+//        char[] word2 = t.toCharArray();
+//
+//        Arrays.sort(word1);
+//        Arrays.sort(word2);
+//
+//
+//        if (Arrays.equals(word1, word2)) {
+//            return true;
+//        }
+//        return false;
+//    }
+
     public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+
+        HashMap<Character,Integer> string1= new HashMap<>();
+        HashMap<Character,Integer> string2 = new HashMap<>();
+
+        if(s.length()!=t.length()){
             return false;
         }
+        for(int i =0;i<s.length();i++){
+            if(string1.containsKey(s.charAt(i))){
+                string1.put(s.charAt(i),string1.get(s.charAt(i))+1);
+            }
+            else{
+                string1.put(s.charAt(i),1);
+            }
 
-        char[] word1 = s.toCharArray();
-        char[] word2 = t.toCharArray();
-
-        Arrays.sort(word1);
-        Arrays.sort(word2);
-
-
-        if (Arrays.equals(word1, word2)) {
-            return true;
+            if(string2.containsKey(t.charAt(i))){
+                string2.put(t.charAt(i),string2.get(t.charAt(i))+1);
+            }
+            else{
+                string2.put(t.charAt(i),1);
+            }
         }
-        return false;
+
+        for(Map.Entry<Character, Integer> val : string1.entrySet()){
+            if(!string2.containsKey(val.getKey())){
+                return false;
+            }
+            else{
+                int tmp = string2.get(val.getKey());
+                if(tmp!=val.getValue()){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
